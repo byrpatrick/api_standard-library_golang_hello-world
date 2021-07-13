3d661a296b57667905523ddbf68ab36a78354fdd
Avnish Pundir <apundir@gmail.com>
2021-07-13 14:29:53 +0530

    starter branch with functional API
    - Tested with Angular SPA app

--- main.go
+++ main.go
@@ -1,39 +1,12 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
-	"strings"
-
-	"github.com/lestrrat-go/jwx/jwk"
-	"github.com/lestrrat-go/jwx/jwt"
 )
 
-const (
-	corsAllowedDomain = "http://localhost:4040"
-	authHeader        = "Authorization"
-	ctxTokenKey       = "Auth0Token"
-)
-
-const (
-	yamlCfgFileName = "env.yaml"
-	domainYamlKey   = "auth0-domain"
-	audienceYamlKey = "auth0-audience"
-)
-
-// variables required from user
-var (
-	auth0Audience string
-	auth0Domain   string
-)
-
-var (
-	tenantKeys jwk.Set
-)
+const corsAllowedDomain = "http://localhost:4040"
 
 type message struct {
 	Message string `json:"message"`
@@ -45,20 +18,26 @@ var (
 	adminMessage     = &message{"The API successfully recognized you as an admin."}
 )
 
-func publicApiHandler(rw http.ResponseWriter, _ *http.Request) {
-	sendMessage(rw, publicMessage)
+func publicApiHandler(rw http.ResponseWriter, req *http.Request) {
+	sendMessage(rw, req, publicMessage)
 }
 
-func protectedApiHandler(rw http.ResponseWriter, _ *http.Request) {
-	sendMessage(rw, protectedMessage)
+func protectedApiHandler(rw http.ResponseWriter, req *http.Request) {
+	sendMessage(rw, req, protectedMessage)
 }
 
-func adminApiHandler(rw http.ResponseWriter, _ *http.Request) {
-	sendMessage(rw, adminMessage)
+func adminApiHandler(rw http.ResponseWriter, req *http.Request) {
+	sendMessage(rw, req, adminMessage)
 }
 
-func sendMessage(rw http.ResponseWriter, data *message) {
-	rw.Header().Add("Content-Type", "application/json")
+func sendMessage(rw http.ResponseWriter, req *http.Request, data *message) {
+	if req.Method == http.MethodOptions {
+		sendOptionsResponse(rw)
+		return
+	}
+	headers := rw.Header()
+	headers.Add("Content-Type", "application/json")
+	headers.Add("Access-Control-Allow-Origin", corsAllowedDomain)
 	bytes, err := json.Marshal(data)
 	if err != nil {
 		log.Print("json conversion error", err)
@@ -70,86 +49,26 @@ func sendMessage(rw http.ResponseWriter, data *message) {
 	}
 }
 
-func handleCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		headers := rw.Header()
-		// Allow-Origin header shall be part of ALL the responses
-		headers.Add("Access-Control-Allow-Origin", corsAllowedDomain)
-		if req.Method != http.MethodOptions {
-			next.ServeHTTP(rw, req)
-			return
-		}
-		// process an HTTP OPTIONS preflight request
-		headers.Add("Access-Control-Allow-Headers", "Authorization")
-		rw.WriteHeader(http.StatusNoContent)
-		if _, err := rw.Write(nil); err != nil {
-			log.Print("http response (options) write error", err)
-		}
-	})
-}
-
-// validateToken middleware verifies a valid Auth0 JWT token being present in the request.
-func validateToken(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		token, err := extractToken(req)
-		if err != nil {
-			fmt.Printf("failed to parse payload: %s\n", err)
-			rw.WriteHeader(http.StatusUnauthorized)
-			sendMessage(rw, &message{err.Error()})
-			return
-		}
-		ctxWithToken := context.WithValue(req.Context(), ctxTokenKey, token)
-		next.ServeHTTP(rw, req.WithContext(ctxWithToken))
-	})
-}
-
-// extractToken parses the Authorization HTTP header for valid JWT token and
-// validates it with AUTH0 JWK keys. Also verifies if the audience present in
-// the token matches with the designated audience as per current configuration.
-func extractToken(req *http.Request) (jwt.Token, error) {
-	authorization := req.Header.Get(authHeader)
-	if authorization == "" {
-		return nil, errors.New("authorization header missing")
+func sendOptionsResponse(rw http.ResponseWriter) {
+	headers := rw.Header()
+	headers.Add("Access-Control-Allow-Origin", corsAllowedDomain)
+	headers.Add("Access-Control-Allow-Headers", "Authorization")
+	rw.WriteHeader(http.StatusNoContent)
+	if _, err := rw.Write(nil); err != nil {
+		log.Print("http response (options) write error", err)
 	}
-	bearerAndToken := strings.Split(authorization, " ")
-	if len(bearerAndToken) < 2 {
-		return nil, errors.New("malformed authorization header: " + authorization)
-	}
-	token, err := jwt.Parse([]byte(bearerAndToken[1]), jwt.WithKeySet(tenantKeys),
-		jwt.WithValidate(true), jwt.WithAudience(auth0Audience))
-	if err != nil {
-		return nil, err
-	}
-	return token, nil
-}
-
-// fetchTenantKeys fetch and parse the tenant JSON Web Keys (JWK). The keys
-// are used for JWT token validation during requests authorization.
-func fetchTenantKeys() {
-	set, err := jwk.Fetch(context.Background(),
-		fmt.Sprintf("https://%s/.well-known/jwks.json", auth0Domain))
-	if err != nil {
-		log.Fatalf("failed to parse tenant json web keys: %s\n", err)
-	}
-	tenantKeys = set
 }
 
 func main() {
-	initConfig()
-	fetchTenantKeys()
-
 	router := http.NewServeMux()
-	router.Handle("/", http.NotFoundHandler())
-	router.Handle("/api/messages/public", http.HandlerFunc(publicApiHandler))
-	router.Handle("/api/messages/protected", validateToken(http.HandlerFunc(protectedApiHandler)))
-	router.Handle("/api/messages/admin", validateToken(http.HandlerFunc(adminApiHandler)))
-	routerWithCORS := handleCORS(router)
+	router.HandleFunc("/api/messages/public", publicApiHandler)
+	router.HandleFunc("/api/messages/protected", protectedApiHandler)
+	router.HandleFunc("/api/messages/admin", adminApiHandler)
 
 	server := &http.Server{
 		Addr:    ":6060",
-		Handler: routerWithCORS,
+		Handler: router,
 	}
 
-	log.Printf("API server listening on %s", server.Addr)
 	log.Fatal(server.ListenAndServe())
 }
