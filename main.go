8c2e9742b3164e06660774ffe24830626a4912df
Avnish Pundir <apundir@gmail.com>
2021-07-15 12:19:36 +0530

    converted CORS handler as middleware

--- main.go
+++ main.go
@@ -18,26 +18,20 @@ var (
 	adminMessage     = &message{"The API successfully recognized you as an admin."}
 )
 
-func publicApiHandler(rw http.ResponseWriter, req *http.Request) {
-	sendMessage(rw, req, publicMessage)
+func publicApiHandler(rw http.ResponseWriter, _ *http.Request) {
+	sendMessage(rw, publicMessage)
 }
 
-func protectedApiHandler(rw http.ResponseWriter, req *http.Request) {
-	sendMessage(rw, req, protectedMessage)
+func protectedApiHandler(rw http.ResponseWriter, _ *http.Request) {
+	sendMessage(rw, protectedMessage)
 }
 
-func adminApiHandler(rw http.ResponseWriter, req *http.Request) {
-	sendMessage(rw, req, adminMessage)
+func adminApiHandler(rw http.ResponseWriter, _ *http.Request) {
+	sendMessage(rw, adminMessage)
 }
 
-func sendMessage(rw http.ResponseWriter, req *http.Request, data *message) {
-	if req.Method == http.MethodOptions {
-		sendOptionsResponse(rw)
-		return
-	}
-	headers := rw.Header()
-	headers.Add("Content-Type", "application/json")
-	headers.Add("Access-Control-Allow-Origin", corsAllowedDomain)
+func sendMessage(rw http.ResponseWriter, data *message) {
+	rw.Header().Add("Content-Type", "application/json")
 	bytes, err := json.Marshal(data)
 	if err != nil {
 		log.Print("json conversion error", err)
@@ -49,21 +43,30 @@ func sendMessage(rw http.ResponseWriter, req *http.Request, data *message) {
 	}
 }
 
-func sendOptionsResponse(rw http.ResponseWriter) {
-	headers := rw.Header()
-	headers.Add("Access-Control-Allow-Origin", corsAllowedDomain)
-	headers.Add("Access-Control-Allow-Headers", "Authorization")
-	rw.WriteHeader(http.StatusNoContent)
-	if _, err := rw.Write(nil); err != nil {
-		log.Print("http response (options) write error", err)
-	}
+func handleCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		headers := rw.Header()
+		// Allow-Origin header shall be part of ALL the responses
+		headers.Add("Access-Control-Allow-Origin", corsAllowedDomain)
+		if req.Method != http.MethodOptions {
+			next.ServeHTTP(rw, req)
+			return
+		}
+		// process an HTTP OPTIONS preflight request
+		headers.Add("Access-Control-Allow-Headers", "Authorization")
+		rw.WriteHeader(http.StatusNoContent)
+		if _, err := rw.Write(nil); err != nil {
+			log.Print("http response (options) write error", err)
+		}
+	})
 }
 
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("/api/messages/public", publicApiHandler)
-	router.HandleFunc("/api/messages/protected", protectedApiHandler)
-	router.HandleFunc("/api/messages/admin", adminApiHandler)
+	router.Handle("/", http.NotFoundHandler())
+	router.Handle("/api/messages/public", handleCORS(http.HandlerFunc(publicApiHandler)))
+	router.Handle("/api/messages/protected", handleCORS(http.HandlerFunc(protectedApiHandler)))
+	router.Handle("/api/messages/admin", handleCORS(http.HandlerFunc(adminApiHandler)))
 
 	server := &http.Server{
 		Addr:    ":6060",
