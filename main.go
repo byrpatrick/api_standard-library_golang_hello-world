bbb2c7fe060a1d039201dcd210c79b76b73244c4
Avnish Pundir <apundir@gmail.com>
2021-07-16 15:08:53 +0530

    Add token validation

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/lestrrat-go/jwx/jwt"
 )
 
-const corsAllowedDomain = "http://localhost:4040"
+const (
+	corsAllowedDomain = "http://localhost:4040"
+	authHeader        = "Authorization"
+	ctxTokenKey       = "Auth0Token"
+)
+
+// variables required from user
+var (
+	auth0Audience string
+	auth0Domain   string
+)
+
+var (
+	tenantKeys jwk.Set
+)
 
 type message struct {
 	Message string `json:"message"`
@@ -61,12 +84,87 @@ func handleCORS(next http.Handler) http.Handler {
 	})
 }
 
+func validateToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		token, err := extractToken(req)
+		if err != nil {
+			fmt.Printf("failed to parse payload: %s\n", err)
+			rw.WriteHeader(http.StatusUnauthorized)
+			sendMessage(rw, &message{err.Error()})
+			return
+		}
+		if !containsValidAudience(token) {
+			rw.WriteHeader(http.StatusUnauthorized)
+			sendMessage(rw, &message{"invalid audience"})
+			return
+		}
+		ctx := context.WithValue(req.Context(), ctxTokenKey, token)
+		next.ServeHTTP(rw, req.WithContext(ctx))
+	})
+}
+
+func containsValidAudience(token jwt.Token) bool {
+	for _, anAudience := range token.Audience() {
+		if anAudience == auth0Audience {
+			return true
+		}
+	}
+	return false
+}
+
+// extractToken parses the Authorization HTTP header for valid JWT token and
+// validates it with AUTH0 JWK keys
+func extractToken(req *http.Request) (jwt.Token, error) {
+	authorization := req.Header.Get(authHeader)
+	if authorization == "" {
+		return nil, errors.New("authorization header missing")
+	}
+	bearerAndToken := strings.Split(authorization, " ")
+	if len(bearerAndToken) < 2 {
+		return nil, errors.New("malformed authorization header: " + authorization)
+	}
+	return jwt.Parse([]byte(bearerAndToken[1]), jwt.WithKeySet(tenantKeys))
+}
+
+// getTenantKeys fetch and parse the tenant JSON Web Keys (JWK). The keys
+// are used for JWT token validation during requests authorization.
+func getTenantKeys() {
+	set, err := jwk.Fetch(context.Background(),
+		fmt.Sprintf("https://%s/.well-known/jwks.json", auth0Domain))
+	if err != nil {
+		log.Fatalf("failed to parse tenant json web keys: %s\n", err)
+	}
+	tenantKeys = set
+}
+
+func exitWithError(message string) {
+	fmt.Fprintf(os.Stderr, "%s\n", message)
+	os.Exit(1)
+}
+
+func parseArgs() {
+	flag.StringVar(&auth0Audience, "a",
+		os.Getenv("AUTH0_AUDIENCE"), "Auth0 API identifier, as audience")
+	flag.StringVar(&auth0Domain, "d",
+		os.Getenv("AUTH0_DOMAIN"), "Auth0 API tenant domain")
+	flag.Parse()
+	if auth0Audience == "" {
+		exitWithError("Auth0 API identifier (as audience) missing")
+	}
+	if auth0Domain == "" {
+		exitWithError("Auth0 API tenant domain missing")
+	}
+}
+
 func main() {
+	parseArgs()
+	getTenantKeys()
+
 	router := http.NewServeMux()
 	router.Handle("/", http.NotFoundHandler())
 	router.Handle("/api/messages/public", http.HandlerFunc(publicApiHandler))
-	router.Handle("/api/messages/protected", http.HandlerFunc(protectedApiHandler))
-	router.Handle("/api/messages/admin", http.HandlerFunc(adminApiHandler))
+	router.Handle("/api/messages/protected", validateToken(http.HandlerFunc(protectedApiHandler)))
+	router.Handle("/api/messages/admin", validateToken(http.HandlerFunc(adminApiHandler)))
 	routerWithCORS := handleCORS(router)
 
 	server := &http.Server{
